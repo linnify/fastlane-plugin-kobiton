2a6855c6db3986a4b5366d24cefa2056e8d5a944
Vlad Rusu <vlad.rusu@linnify.com>
2020-07-17 11:03:52 +0300

    reimprove formatting

--- lib/fastlane/plugin/kobiton/actions/kobiton_action.rb
+++ lib/fastlane/plugin/kobiton/actions/kobiton_action.rb
@@ -10,8 +10,8 @@ module Fastlane
         username = params[:username]
         api_key = params[:api_key]
 
-        base64Authorization = Base64.encode64("#{username}:#{api_token}")
-        authorization = "Basic #{base64authorization}"
+        base64_authorization = Base64.encode64("#{username}:#{api_key}")
+        authorization = "Basic #{base64_authorization}"
 
         filepath = params[:file]
         app_id = params[:app_id]
@@ -40,7 +40,6 @@ module Fastlane
         self.notify_kobiton_after_file_upload(app_path, filename, authorization)
 
         UI.message("Successfully uploaded the build to Kobiton!")
-
       end
 
       def self.description
@@ -67,7 +66,7 @@ module Fastlane
             env_name: "FL_KOBITON_API_KEY",
             description: "API key from Kobiton",
             verify_block: proc do |value|
-              UI.user_error!("No API key for KobitonUpload given, pass using `api_key: 'token'`") unless (value and not value.empty?)
+              UI.user_error!("No API key for KobitonUpload given, pass using `api_key: 'token'`") unless value && !value.empty?
             end,
             optional: false,
             type: String
@@ -77,7 +76,7 @@ module Fastlane
             env_name: "FL_KOBITON_USERNAME",
             description: "The username or email of your Kobiton account",
             verify_block: proc do |value|
-              UI.user_error!("No username/email for KobitonUpload given, pass using `username: 'username/email'`") unless (value and not value.empty?)
+              UI.user_error!("No username/email for KobitonUpload given, pass using `username: 'username/email'`") unless value && !value.empty?
             end,
             optional: false,
             type: String
@@ -87,7 +86,7 @@ module Fastlane
             env_name: "FL_KOBITON_FILE",
             description: "The build file to upload to Kobiton",
             verify_block: proc do |value|
-              UI.user_error!("No build file for KobitonUpload given, pass using `file: 'file_path'`") unless (value and not value.empty?)
+              UI.user_error!("No build file for KobitonUpload given, pass using `file: 'file_path'`") unless value && !value.empty?
             end,
             optional: false,
             type: String
@@ -97,7 +96,7 @@ module Fastlane
             env_name: "FL_KOBITON_APP_ID",
             description: "The Kobiton app ID of the application",
             verify_block: proc do |value|
-              UI.user_error!("No app ID or value 0 for KobitonUpload given, pass using `app_id: <app_id>`") unless (value and value != 0)
+              UI.user_error!("No app ID or value 0 for KobitonUpload given, pass using `app_id: <app_id>`") unless value && value != 0
             end,
             optional: false,
             type: Integer
@@ -119,10 +118,10 @@ module Fastlane
           "Accept" => "application/json"
         }
 
-        response = RestClient.post "https://api.kobiton.com/v1/apps/uploadUrl", {
+        response = RestClient.post("https://api.kobiton.com/v1/apps/uploadUrl", {
           "filename" => filename,
           "appId" => app_id
-        }, headers
+        }, headers)
 
         return JSON.parse(response)
       end
@@ -135,7 +134,7 @@ module Fastlane
           "x-amz-tagging" => "unsaved=true"
         }
 
-        response = RestClient.put url, File.read(filepath), headers
+        response = RestClient.put(url, File.read(filepath), headers)
 
         return response.code == 200
       end
@@ -148,10 +147,10 @@ module Fastlane
           "Content-Type" => "application/json"
         }
 
-        response = RestClient.post "https://api.kobiton.com/v1/apps", {
+        RestClient.post("https://api.kobiton.com/v1/apps", {
           "filename" => filename,
           "appPath" => app_path
-        }, headers
+        }, headers)
       end
     end
   end
