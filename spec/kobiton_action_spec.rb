59469bed762c94394606f1bdea564684b1be983d
Vlad Rusu <vlad.rusu@linnify.com>
2020-07-29 18:49:12 +0300

    increase version number

--- spec/kobiton_action_spec.rb
+++ spec/kobiton_action_spec.rb
@@ -4,6 +4,6 @@ describe Fastlane::Actions::KobitonAction do
       # expect(Fastlane::UI).to receive(:message).with("The kobiton plugin is working!")
 
       # Fastlane::Actions::KobitonAction.run(nil)
-    end
+      end
   end
 end
