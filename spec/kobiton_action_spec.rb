ba408cff38ad4d7ee683a5a3ffd5c55bc9f1fb3f
Vlad Rusu <vladr11@users.noreply.github.com>
2021-02-26 10:47:44 +0200

    Fix spec identation (#2)
    * fixed identation in spec file
    
    * remove commented statements
    
    * remove empty line in block

--- spec/kobiton_action_spec.rb
+++ spec/kobiton_action_spec.rb
@@ -1,9 +1,6 @@
 describe Fastlane::Actions::KobitonAction do
   describe '#run' do
     it 'prints a message' do
-      # expect(Fastlane::UI).to receive(:message).with("The kobiton plugin is working!")
-
-      # Fastlane::Actions::KobitonAction.run(nil)
-      end
+    end
   end
 end
